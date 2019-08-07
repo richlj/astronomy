0773475dd7a0431dc42a4e355f5e57d929164969
Richard Jackson <rich@richlj.com>
2019-08-07 03:57:39 +0100

    adding almostEqual to enable comparison between float64s in tests

--- functions_test.go
+++ functions_test.go
@@ -8,3 +8,8 @@ import (
 
 // tolerance is used for comparing float values in tests
 var tolerance = math.Pow(10, -6)
+
+// almostEqual compares the values of two float64s within a set parameter
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= tolerance
+}
