d844aeec8ed3d84b91e099bd5fa8e5767ff08eeb
Richard Jackson <rich@richlj.com>
2019-08-07 04:13:31 +0100

    adding Location type

--- types.go
+++ types.go
@@ -12,3 +12,11 @@ type julianTime float64
 
 // Altitude is the height in meters of an object above sea level
 type Altitude float64
+
+// Location is the three-dimensional position of an object above the globe.
+// Latitude and Longitude values are in degrees.
+type Location struct {
+	Latitude  float64  `json:"latitude" validate:"min=-90,max=90"`
+	Longitude float64  `json:"longitude" validate:"min=-180,max=180"`
+	Altitude  Altitude `json:"altitude" validate:"min=0"`
+}
