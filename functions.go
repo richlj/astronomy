855cc7f8cecd9bad7295045a5b2185946607484c
Richard Jackson <rich@richlj.com>
2019-08-07 03:45:02 +0100

    adding trig functions for use with angles in degrees

---
+++ functions.go
@@ -0,0 +1,27 @@
+// Package astro contains functions for calculating astronomical times and
+// positions
+package astro
+
+import (
+	"math"
+)
+
+// sin provides the Sine of an angle that is provided in degress
+func sin(a float64) float64 {
+	return math.Sin(a / 180 * math.Pi)
+}
+
+// cos provides the Cosine of an angle that is provided in degress
+func cos(a float64) float64 {
+	return math.Cos(a / 180 * math.Pi)
+}
+
+// asin provides the arcsine in degress of the supplied value
+func asin(a float64) float64 {
+	return math.Asin(a) * 180 / math.Pi
+}
+
+// acos provides the arccosine in degress of the supplied value
+func acos(a float64) float64 {
+	return math.Acos(a) * 180 / math.Pi
+}
